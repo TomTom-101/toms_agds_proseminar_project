9b98b593ec0664637cdabadc899073112ae43349
Thomas Jucker <jucker_thomas@bluewin.ch>
2025-10-28 22:08:51 +0100

    replaced mean delay in minutes by mean delay rate, being fraction of trains at each stop that were delayed

--- R/punctuality_map.R
+++ R/punctuality_map.R
@@ -19,51 +19,55 @@ punctuality_2025_10_11_geo <- punctuality_2025_10_11_geo %>%
     N_Koordinate = as.numeric(`N-Koordinate`)
   )
 
-# Calculate mean punctuality (delay) per stop (only delay upon arrival)
-mean_delay_per_stop <- punctuality_2025_10_11_geo %>%
+
+# Calculate delay rate per stop
+delay_rate_per_stop <- punctuality_2025_10_11_geo %>%
   group_by(BPUIC, HALTESTELLEN_NAME) %>%
   summarise(
-    mean_delay_min = mean(c(diff_arr), na.rm = TRUE),
+    n_trains = n(),
+    n_delayed = sum(punct_cat == "delayed", na.rm = TRUE),
+    delay_rate = n_delayed / n_trains,  # fraction of delayed trains
     .groups = "drop"
   )
 
-# Join with stop coordinates (keep coordinates and metadata)
-mean_delay_geo <- mean_delay_per_stop %>%
-  left_join(
+# join with coordinates
+delay_geo <- delay_rate_per_stop %>%
+  inner_join(
     stops_selected %>%
       mutate(E_Koordinate = as.numeric(`E-Koordinate`),
              N_Koordinate = as.numeric(`N-Koordinate`)),
     by = "BPUIC"
   )
 
-# --- 2. Convert to sf object ---
-mean_delay_sf <- st_as_sf(
-  mean_delay_geo,
+# convert to sf
+delay_sf <- st_as_sf(
+  delay_geo,
   coords = c("E_Koordinate", "N_Koordinate"),
   crs = 2056,
   remove = FALSE
 )
 
+# Reproject to WGS84 for leaflet
+delay_sf_wgs <- st_transform(delay_sf, crs = 4326)
 
-# Reproject to WGS84 (required for annotation_map_tile)
-mean_delay_sf_wgs <- st_transform(mean_delay_sf, crs = 4326)
 
-# Create Leaflet map
-leaflet(mean_delay_sf_wgs) %>%
-  addProviderTiles(providers$CartoDB.Positron) %>%  # light grey basemap
+# plot
+leaflet(delay_sf_wgs) %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%  # grey basemap
   addCircleMarkers(
     radius = 4,
-    color = ~viridis::viridis(100, option = "plasma")[cut(mean_delay_min, breaks = 100)],
+    color = ~viridis(100, option = "plasma")[cut(delay_rate, breaks = 100)],
     stroke = FALSE,
     fillOpacity = 0.8,
     popup = ~paste0(
       "<b>", HALTESTELLEN_NAME, "</b><br>",
-      "Mean delay: ", round(mean_delay_min, 1), " min"
+      "Delay rate: ", round(delay_rate * 100, 1), " %<br>",
+      "Trains delayed: ", n_delayed, " / ", n_trains
     )
   ) %>%
   addLegend(
     position = "bottomright",
-    pal = colorNumeric(palette = "plasma", domain = mean_delay_sf_wgs$mean_delay_min),
-    values = mean_delay_sf_wgs$mean_delay_min,
-    title = "Mean Delay (min)"
+    pal = colorNumeric(palette = "plasma", domain = delay_sf_wgs$delay_rate),
+    values = delay_sf_wgs$delay_rate,
+    title = "Delay rate"
   )
